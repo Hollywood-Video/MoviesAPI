f9e673c012f673503a4e64a59a4e768bf3dd9663
felipe d ferreira <felipedferreira11@gmail.com>
2025-11-02 01:24:34 -0500

    chore: create movie endpoint and request is added

--- src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieEndpoint.cs
+++ src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieEndpoint.cs
@@ -1,6 +1,19 @@
 ﻿namespace Prometheus.MovieBuff.Presentation.Features.Movies.CreateMovie.v1;
 
-public class CreateMovieEndpoint
+internal static class CreateMovieEndpoint
 {
-    
+    internal static IEndpointRouteBuilder MapCreateMovieEndpoint(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/movies", async (CreateMovieRequest request, CancellationToken cancellationToken = default) =>
+        {
+            var id = Guid.NewGuid();
+            var result = new { id = id };
+            // TODO => return Results.CreatedAtRoute("", )
+            return Results.Created($"/movies/{id}", result);
+        })
+        .WithName("CreateMovie")
+        .WithTags("Movies");
+
+        return app;
+    }
 }
--- src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieRequest.cs
+++ src/Prometheus.MovieBuff.Presentation/Features/Movies/CreateMovie/v1/CreateMovieRequest.cs
@@ -1,6 +1,4 @@
 ﻿namespace Prometheus.MovieBuff.Presentation.Features.Movies.CreateMovie.v1;
 
-public class CreateMovieRequest
-{
-    
-}
+// consider using a class with data annotations for validation
+public record CreateMovieRequest(string Title, int YearOfRelease, IEnumerable<string> Genres);
